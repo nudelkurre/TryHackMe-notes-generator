4b5c83a6d4dd06a672ed0d891a826dd5453d6d16
Emil <nudelkurre@protonmail.com>
2021-10-16 14:46:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# THM notes generator
+
+A tool to generate a template to use for taking notes with TryHackMe rooms
+
+```
+  -h, --help  show this help message and exit
+  -u URL      Specify an url to a room
+  -n NAME     Specify a name to put in the notes
+```
---
+++ thm-notes.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import requests
+from datetime import datetime
+import argparse
+import re
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", type=str, dest="url", required=True, help="Specify an url to a room")
+parser.add_argument("-n", dest="name", help="Specify a name to put in the notes")
+args = parser.parse_args()
+
+tags = re.compile(r'(<!--.*?-->|<[^>]*>)')
+
+def url_to_room(url):
+    room = url.split("/")[-1]
+    url = "/".join(url.split("/")[:-1])
+    if(url == "https://tryhackme.com/room"):
+        return room
+    else:
+        print("Not an url to a THM room")
+        return
+
+def get_details(room):
+    return requests.get(f"https://tryhackme.com/api/room/details?codes={room}").json()[room]
+
+def get_tasks(room):
+    return requests.get(f"https://tryhackme.com/api/tasks/{room}").json()['data']
+
+def get_date():
+    return datetime.now().strftime("%B %d, %Y")
+
+def format_question(question):
+    return(tags.sub("","".join(f"\n```\n{question['question']}\n\n> \n```\n".replace("<p>", "").replace("</p>", ""))))
+
+def write_notes(room):
+    details = get_details(room)
+    title = details['title']
+    with open(f"{title}.md".replace(" ", ""), "w") as f:
+        f.write(f"# {title}\n")
+        if(args.name):
+            f.write(f"{args.name}\n")
+        f.write(f"> {get_date()}\n")
+        for t in get_tasks(room):
+            f.write(f"---\n## TASK {t['taskNo']}\n")
+            for q in t['questions']:
+                f.write(f"{format_question(q)}")
+            
+room = url_to_room(args.url)
+if(room):
+    write_notes(room)
